1a6f4ec13d8ff17c1d901cb15837c6b775df80df
Daniel Dziura <danieldziura@paucasesnovescifp.cat>
2020-10-22 18:54:32 +0200

    Ejercicio foreach and List
    Se crea dos listas una de string y otra de objetos.

--- Listas/Program.cs
+++ Listas/Program.cs
@@ -10,6 +10,45 @@ namespace Listas
     {
         static void Main(string[] args)
         {
+            List<string> people = GetPeopleStrings();
+
+            foreach (string person in people)
+            {
+                Console.WriteLine(person);    
+            }
+
+            Console.ReadLine();
+
+            List<PersonModel> peopleModels = GetPeopleModels();
+
+            foreach (PersonModel person in peopleModels)
+            {
+                Console.WriteLine($"Hello { person.FirstName} { person.LastName }");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static List<PersonModel> GetPeopleModels()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+
+            output.Add(new PersonModel { FirstName = "Tim", LastName = "Co" });
+            output.Add(new PersonModel { FirstName = "Dani", LastName = "AA" });
+
+            return output;
+        }
+
+        private static List<string> GetPeopleStrings()
+        {
+            List<string> output = new List<string>();
+
+            output.Add("Dani");
+            output.Add("Adri");
+            output.Add("222");
+
+            return output;
+
         }
     }
 }
